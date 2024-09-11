8cd441ed3d3c31ba67b42aab8453884b3df1d65d
KeerthiKonduru04 <keerthivempalli04gmail.com>
2024-09-11 13:50:54 +0530

    keerthi

--- src/test/java/org/test/SampleTest.java
+++ src/test/java/org/test/SampleTest.java
@@ -3,6 +3,8 @@ package org.test;
 public class SampleTest {
 public static void main(String[] args) {
 	System.out.println("Hello World");
-	
+	System.out.println("Test1");
+	System.out.println("Test2");
+	System.out.println("Test3");
 }
 }
