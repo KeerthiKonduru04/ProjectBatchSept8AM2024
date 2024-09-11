c112f6ac742eab5114e249112f425eb87b5217bd
swethaambika <swethaagalya2020@gmail.com>
2024-09-11 13:49:14 +0530

    swetha code

--- src/test/java/org/test/SampleTest.java
+++ src/test/java/org/test/SampleTest.java
@@ -3,6 +3,9 @@ package org.test;
 public class SampleTest {
 public static void main(String[] args) {
 	System.out.println("Hello World");
+	System.out.println("swetha");
+	System.out.println("max");
+	System.out.println("kavin");
 	
 }
 }
